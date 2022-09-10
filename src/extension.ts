d73b9b8ca96552b309f85396283e582a8e055fd1
dragonmux <git@dragonmux.network>
2022-09-10 22:53:38 +0100

    extension: Begun implementing the extension top-level so it can start to do things

---
+++ src/extension.ts
@@ -0,0 +1,133 @@
+import
+{
+	commands,
+	Disposable,
+	ExtensionContext,
+	languages,
+	TextEditor,
+	Uri,
+	window,
+	workspace,
+	WorkspaceFolder,
+	WorkspaceFoldersChangeEvent
+} from "vscode";
+import * as langClient from "vscode-languageclient";
+import {Observable} from "./utils/observable";
+
+export interface Api
+{
+	activeWorkspace : typeof activeWorkspace
+}
+
+export async function activate(context: ExtensionContext) : Promise<Api>
+{
+	context.subscriptions.push(
+		...[
+			configureLanguage(),
+			...registerCommands(),
+			workspace.onDidChangeWorkspaceFolders(workspaceFoldersChanged),
+			window.onDidChangeActiveTextEditor(activeTextEditorChanged),
+		],
+	)
+	activeTextEditorChanged(window.activeTextEditor)
+
+	return {activeWorkspace}
+}
+
+export async function deactivate()
+{
+	return Promise.all([...workspaces.values()].map(workspace => workspace.stop()));
+}
+
+const workspaces: Map<string, ClientWorkspace> = new Map()
+const activeWorkspace = new Observable<ClientWorkspace | null>(null)
+
+export class ClientWorkspace
+{
+	public readonly folder: WorkspaceFolder
+	private languageClient: langClient.CommonLanguageClient | null = null
+	private disposables: Disposable[]
+
+	constructor(folder: WorkspaceFolder)
+	{
+		this.folder = folder
+		this.disposables = []
+	}
+
+	public async autoStart()
+	{
+		return this.start().then(() => true)
+	}
+
+	public async start()
+	{
+		//const client = await createLanguageClient
+	}
+
+	public async stop()
+	{
+		if (this.languageClient)
+			await this.languageClient.stop()
+		this.disposables.forEach(item => void item.dispose())
+	}
+
+	public async restart()
+	{
+		await this.stop()
+		return this.start()
+	}
+}
+
+function activeTextEditorChanged(editor: TextEditor | undefined)
+{
+	if (!editor || !editor.document)
+		return
+	const {languageId, uri} = editor.document
+	const workspace = clientWorkspaceForURI(uri, {initialiseIfMissing: languageId === "mangrove"})
+	if (!workspace)
+		return
+	activeWorkspace.value = workspace
+}
+
+function workspaceFoldersChanged(event: WorkspaceFoldersChangeEvent)
+{
+	for (const folder of event.removed)
+	{
+		const workspace = workspaces.get(folder.uri.toString())
+		if (workspace)
+		{
+			workspaces.delete(folder.uri.toString())
+			workspace.stop()
+		}
+	}
+}
+
+function clientWorkspaceForURI(uri: Uri, options?: {initialiseIfMissing: boolean}): ClientWorkspace | undefined
+{
+	const folder = workspace.getWorkspaceFolder(uri)
+	if (!folder)
+		return undefined
+
+	const alreadyExists = workspaces.get(folder.uri.toString())
+	if (!alreadyExists && options && options.initialiseIfMissing)
+	{
+		const workspace = new ClientWorkspace(folder)
+		workspaces.set(folder.uri.toString(), workspace)
+		workspace.autoStart()
+	}
+	return workspaces.get(folder.uri.toString())
+}
+
+function registerCommands(): Disposable[]
+{
+	return [
+		commands.registerCommand('mangrove.restart',
+			async () => activeWorkspace.value?.restart()
+		),
+	];
+}
+
+function configureLanguage(): Disposable
+{
+	return languages.setLanguageConfiguration("mangrove", {})
+}
---
+++ src/utils/observable.ts
@@ -0,0 +1,40 @@
+import {Disposable} from "vscode"
+
+/**
+ * A wrapper around a value of type `T` that can be subscribed to whenever the
+ * underlying value changes.
+ */
+export class Observable<T>
+{
+	private _listeners: Set<(arg: T) => void> = new Set()
+	private _value: T
+
+	/** Returns the current value. */
+	get value()
+	{
+		return this._value
+	}
+
+	/** Every change to the value triggers all the registered callbacks. */
+	set value(value: T)
+	{
+		this._value = value
+		this._listeners.forEach(fn => fn(value))
+	}
+
+	constructor(value: T)
+	{
+		this._value = value
+	}
+
+	/**
+	 * Registers a listener function that's called whenever the underlying value
+	 * changes.
+	 * @returns a function that unregisters the listener when called.
+	 */
+	public observe(fn: (arg: T) => void): Disposable
+	{
+		this._listeners.add(fn)
+		return {dispose: () => this._listeners.delete(fn)}
+	}
+}
