1fdfbbc0a184907f9d4dab9244e61c6ace0d6eea
dragonmux <git@dragonmux.network>
2022-09-11 23:13:39 +0100

    client: More work on progress indications for thee status bar

--- src/client/mangrove.ts
+++ src/client/mangrove.ts
@@ -1,11 +1,15 @@
 import {workspace, WorkspaceFolder} from 'vscode'
 import * as path from 'path'
 import * as langClient from 'vscode-languageclient'
-import {DocumentSelector, LanguageClient, ServerOptions, TransportKind} from 'vscode-languageclient/node'
+import {
+	DocumentSelector,
+	LanguageClient,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node'
 import {extensionContext, WorkspaceProgress} from './extension'
 import {Observable} from './utils/observable'
 
-const progress: Observable<WorkspaceProgress> = new Observable<WorkspaceProgress>({state: 'standby'})
 export const documentSelector: DocumentSelector = [
 	{language: 'mangrove', scheme: 'file'},
 	{language: 'mangrove', scheme: 'untitled'},
@@ -37,18 +41,54 @@ export async function createLanguageClient(folder: WorkspaceFolder): Promise<Lan
 		}
 	}
 
-	const instance = new LanguageClient(
+	return new LanguageClient(
 		'mangrove-client',
 		'Mangrove Language Server',
 		serverOptions,
 		clientOptions
 	)
-	instance.registerProposedFeatures()
-	return instance
 }
 
-export function setupProgress(_client: langClient.CommonLanguageClient, workspaceProgress: Observable<WorkspaceProgress>)
+interface ProgressParams
 {
-	workspaceProgress.value = progress.value
-	progress.observe(progress => {workspaceProgress.value = progress})
+	id: string
+	title?: string
+	message?: string
+	percentage?: number
+	done?: boolean
+}
+
+export function setupProgress(client: langClient.CommonLanguageClient, workspaceProgress: Observable<WorkspaceProgress>)
+{
+	const runningProgress: Set<string> = new Set()
+
+	client.onReady().then(() =>
+		client.onNotification(
+			new langClient.NotificationType<ProgressParams>('window/progress'),
+			progress =>
+			{
+				if (progress.done)
+					runningProgress.delete(progress.id)
+				else
+					runningProgress.add(progress.id)
+
+				if (runningProgress.size)
+				{
+					const status = (() =>
+					{
+						if (typeof progress.percentage === 'number')
+							return `${Math.round(progress.percentage * 100)}%`
+						else if (progress.message)
+							return progress.message
+						else if (progress.title)
+							return progress.title
+						return ''
+					})()
+					workspaceProgress.value = {state: 'progress', message: status}
+				}
+				else
+					workspaceProgress.value = {state: 'ready'}
+			}
+		)
+	)
 }
