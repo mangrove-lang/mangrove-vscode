3053ec5eb5dda850b86d062a0889f5a08751d28b
dragonmux <git@dragonmux.network>
2022-09-12 00:43:53 +0100

    server/parser: Implemented additonal recogniser routines for various parts of the syntax

--- src/server/parser/recogniser.ts
+++ src/server/parser/recogniser.ts
@@ -1,4 +1,25 @@
-export function isNewLine(c: string)
+export function isNewLine(c: string) { return c == '\n' || c == '\r' }
+export function isWhiteSpace(c: string) { return c == ' ' || c == '\t' || isNewLine(c) }
+
+
+export function isHex(c: string)
 {
-	return c == '\n' || c == '\r'
+	return (c >= '0' && c <= '9') ||
+		(c >= 'A' && c <= 'F') ||
+		(c >= 'a' && c <= 'f')
 }
+
+export function isDot(c: string) { return c == '.' }
+
+export function isNormalAlpha(c: string)
+{
+	return c == ' ' || c == '!' ||
+		(c >= '#' && c <= '&') ||
+		(c >= '(' && c <= '[') ||
+		(c >= ']' && c <= '~') ||
+		(c >= '\u0080' && c <= '\uD7FF') ||
+		(c >= '\uE000' && c <= '\u0010FFFF')
+}
+
+export function isSingleQuote(c: string) { return c == '\'' }
+export function isDoubleQuote(c: string) { return c == '"' }
