55d39c82edede5081f2a7a515eaf2e6dbec6bfbf
dragonmux <git@dragonmux.network>
2022-09-11 21:22:22 +0100

    server/parser: Implemented a set of tokenisation types

---
+++ src/server/parser/types.ts
@@ -0,0 +1,129 @@
+import {Position, Range, TextDocument} from 'vscode-languageserver-textdocument'
+
+export enum TokenType
+{
+	invalid,
+	eof,
+	whitespace,
+	comment,
+	newline,
+	dot,
+	semi,
+	ident,
+	leftParen,
+	rightParen,
+	leftBrace,
+	rightBrace,
+	leftSquare,
+	rightSquare,
+	comma,
+	colon,
+	binLit,
+	octLit,
+	hexLit,
+	intLit,
+	stringLit,
+	charLit,
+	boolLit,
+	nullptrLit,
+	invert,
+	incOp,
+	mulOp,
+	addOp,
+	shiftOp,
+	bitOp,
+	relOp,
+	equOp,
+	logicOp,
+
+	locationSpec,
+	storageSpec,
+	type,
+	assignOp,
+
+	deleteStmt,
+	newStmt,
+	returnStmt,
+	ifStmt,
+	elifStmt,
+	elseStmt,
+	whileStmt,
+	doStmt,
+
+	noneType,
+	arrow,
+	classDef,
+	functionDef,
+	operatorDef,
+	decorator,
+	visibility
+}
+
+export class Token
+{
+	private _type: TokenType = TokenType.invalid
+	private _value = ''
+	private _location: Range = {start: {line: -1, character: -1}, end: {line: -1, character: -1}}
+	private _length = 0
+
+	get type()
+	{
+		return this._type
+	}
+
+	get value()
+	{
+		return this._value
+	}
+
+	get location()
+	{
+		return this._location
+	}
+
+	get length()
+	{
+		return this._length
+	}
+
+	get valid()
+	{
+		return this._type != TokenType.invalid
+	}
+
+	public set(type: TokenType, value?: string)
+	{
+		this._type = type
+		this._value = value ? value : ''
+	}
+
+	public reset()
+	{
+		this._type = TokenType.invalid
+		this._value = ''
+		this._location = {start: {line: -1, character: -1}, end: {line: -1, character: -1}}
+		this._length = 0
+	}
+
+	public beginsAt(position: Position)
+	{
+		this._location.start = position
+	}
+
+	public endsAt(position: Position)
+	{
+		this._location.end = position
+	}
+
+	public calcLength(file: TextDocument)
+	{
+		const beginOffset = file.offsetAt(this._location.start)
+		const endOffset = file.offsetAt(this._location.end)
+		this._length = endOffset - beginOffset
+	}
+
+	public typeIs(...types: TokenType[])
+	{
+		return types.some(type => this._type == type, this)
+	}
+}
