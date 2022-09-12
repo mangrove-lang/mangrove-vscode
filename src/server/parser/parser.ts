7266d25fc48f4a1da4a20acad14e0e1217db4a3e
dragonmux <git@dragonmux.network>
2022-09-12 01:14:31 +0100

    server/parser: Expanded out the parser to start handling things like string literals

--- src/server/parser/parser.ts
+++ src/server/parser/parser.ts
@@ -11,6 +11,19 @@ export class Parser
 		this.lexer = new Tokeniser(file)
 	}
 
+	*match(tokenType: TokenType): Generator<Token, boolean, undefined>
+	{
+		const token = this.lexer.token
+		if (token.typeIs(tokenType))
+		{
+			this.lexer.next()
+			yield *this.skipWhite()
+			return true
+		}
+		//expected(tokenType, token)
+		return false
+	}
+
 	*skipWhite(): Generator<Token, void, undefined>
 	{
 		const token = this.lexer.token
@@ -22,9 +35,66 @@ export class Parser
 		}
 	}
 
-	parseStatement()
+	*parseStringLiteral(): Generator<Token, void, undefined>
 	{
-		//return this.parseExpression()
+		const token = this.lexer.token
+		if (!token.typeIs(TokenType.stringLit))
+			return
+		while (token.typeIs(TokenType.stringLit))
+		{
+			yield token
+			this.match(TokenType.stringLit)
+		}
+	}
+
+	*parseConst(): Generator<Token, boolean, undefined>
+	{
+		const token = this.lexer.token
+		if (token.typeIs(TokenType.invalid))
+			throw Error('Contant expected, got invalid token instead')
+		else if (token.typeIs(TokenType.stringLit))
+			yield *this.parseStringLiteral()
+		else
+		{
+			return false
+		}
+		return true
+	}
+
+	*parseValue(): Generator<Token, boolean, undefined>
+	{
+		//const token = this.lexer.token
+		//if (this.haveIdent())
+		//	yield token
+		//	return
+		const const_ = yield *this.parseConst()
+		if (const_)
+			return true
+		return false
+	}
+
+	*parseExpression(): Generator<Token, boolean, undefined>
+	{
+		const expr = yield *(function *(self): Generator<Token, boolean, undefined>
+		{
+			//const token = this.lexer.token
+			const expr = yield *self.parseValue()
+			return expr
+		})(this)
+		const token = this.lexer.token
+		if (expr && token.typeIs(TokenType.semi))
+			yield *this.match(TokenType.semi)
+		return expr
+	}
+
+	*parseStatement(): Generator<Token, boolean, undefined>
+	{
+		return yield *this.parseExpression()
+	}
+
+	*parseExtStatement(): Generator<Token, boolean, undefined>
+	{
+		return yield *this.parseStatement()
 	}
 
 	public *tokenise(): Generator<Token, void, undefined>
@@ -34,7 +104,9 @@ export class Parser
 			yield token
 		while (!token.typeIs(TokenType.eof))
 		{
-			this.lexer.next()
+			const stmt = yield *this.parseExtStatement()
+			if (!stmt)
+				break
 		}
 	}
 }
