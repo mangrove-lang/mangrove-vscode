9fb20ab2fb306aed2aa09518fa4819e720e0e42e
dragonmux <git@dragonmux.network>
2022-09-12 14:10:39 +0100

    server/parser: Added recognisers for alpha-numerics and underscores

--- src/server/parser/recogniser.ts
+++ src/server/parser/recogniser.ts
@@ -1,6 +1,21 @@
 export function isNewLine(c: string) { return c == '\n' || c == '\r' }
 export function isWhiteSpace(c: string) { return c == ' ' || c == '\t' || isNewLine(c) }
 
+export function isAlpha(c: string)
+{
+	return (c >= 'a' && c <= 'z') ||
+		(c >= 'A' && c <= 'Z') ||
+		(c >= '\u00C0' && c <= '\u2000') ||
+		(c >= '\u2070' && c <= '\uD7FF') ||
+		(c >= '\uE000' && c <= '\uFE4F') ||
+		(c >= '\uFE70' && c <= '\uFEFF') ||
+		(c >= '\u{00010000}' && c <= '\u{0002FA1F}')
+}
+
+export function isDigit(c: string) { return c >= '0' && c <= '9' }
+export function isAlphaNum(c: string) { return isAlpha(c) || isDigit(c) }
+export function isUnderscore(c: string) { return c == '_' }
+
 
 export function isHex(c: string)
 {
@@ -18,7 +33,7 @@ export function isNormalAlpha(c: string)
 		(c >= '(' && c <= '[') ||
 		(c >= ']' && c <= '~') ||
 		(c >= '\u0080' && c <= '\uD7FF') ||
-		(c >= '\uE000' && c <= '\u0010FFFF')
+		(c >= '\uE000' && c <= '\u{0010FFFF}')
 }
 
 export function isSingleQuote(c: string) { return c == '\'' }
