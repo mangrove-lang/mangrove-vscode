94e55c7de74d0687bd5a11dfe02d8e8477eb3384
dragonmux <git@dragonmux.network>
2022-10-10 04:21:35 +0100

    server/ast: Implemented an operator node for between expressions

--- src/server/ast/operations.ts
+++ src/server/ast/operations.ts
@@ -31,3 +31,40 @@ export class ASTRel extends ASTNodeData implements ASTNode
 			yield *child.semanticTokens()
 	}
 }
+
+export class ASTBetween extends ASTNodeData implements ASTNode
+{
+	private _lhs: ASTNode
+	private _lop: Token
+	private _value: ASTNode
+	private _rop: Token
+	private _rhs: ASTNode
+
+	constructor(relation: ASTRel, rop : Token, rhs: ASTNode)
+	{
+		super(relation.rhs.token)
+		this._lhs = relation.lhs
+		this._lop = relation.token
+		this._value = relation.rhs
+		// The parent constructor clones the provided Token, undo that here
+		this._token = this._value.token
+		this._rop = rop
+		this._rhs = rhs
+	}
+
+	get type() { return ASTType.between }
+	get valid() { return this._lhs.valid && this._value.valid && this._rhs.valid }
+	get semanticType() { return SemanticTokenTypes.operator }
+	toString() { return `<Between op: '${this._lop.value} ${this.token.value} ${this._rop.value}'>` }
+
+	*semanticTokens(): Generator<SemanticToken, void, undefined>
+	{
+		yield *this._lhs.semanticTokens()
+		yield this.buildSemanticToken(this.semanticType, this._lop)
+		yield *this._value.semanticTokens()
+		yield this.buildSemanticToken(this.semanticType, this._rop)
+		yield *this._rhs.semanticTokens()
+		for (const child of this.children)
+			yield *child.semanticTokens()
+	}
+}
