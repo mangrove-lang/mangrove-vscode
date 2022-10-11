8d5bb11bb28a8c4de9f581419f7d328521c0e1da
dragonmux <git@dragonmux.network>
2022-10-11 23:15:04 +0100

    server/ast: Implemented an AST type for `elif` expressions

--- src/server/ast/statements.ts
+++ src/server/ast/statements.ts
@@ -28,3 +28,30 @@ export class ASTIfExpr extends ASTNodeData implements ASTNode
 			yield *child.semanticTokens()
 	}
 }
+
+export class ASTElifExpr extends ASTNodeData implements ASTNode
+{
+	private _cond: ASTNode
+	private _trueBlock: ASTNode
+
+	constructor(elifToken: Token, cond: ASTNode, trueBlock: ASTNode)
+	{
+		super(elifToken)
+		this._cond = cond
+		this._trueBlock = trueBlock
+	}
+
+	get type() { return ASTType.elifExpr }
+	get valid() { return this.token.valid && this._cond.valid && this._trueBlock.valid }
+	get semanticType() { return SemanticTokenTypes.keyword }
+	toString() { return '<Elif expression>' }
+
+	*semanticTokens(): Generator<SemanticToken, void, undefined>
+	{
+		yield this.buildSemanticToken(this.semanticType)
+		yield *this._cond.semanticTokens()
+		yield *this._trueBlock.semanticTokens()
+		for (const child of this.children)
+			yield *child.semanticTokens()
+	}
+}
