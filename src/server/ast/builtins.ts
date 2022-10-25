aab1df18ef240f02394b82712613739e8b53f157
dragonmux <git@dragonmux.network>
2022-10-25 11:41:27 +0100

    server/ast: Added 'Bool' to the set of built-in types

--- src/server/ast/builtins.ts
+++ src/server/ast/builtins.ts
@@ -3,10 +3,11 @@ import {MangroveSymbol, SymbolTable, SymbolType, SymbolTypes} from './symbolTabl
 const builtinTypes =
 [
 	new MangroveSymbol('type', new SymbolType(SymbolTypes.type)),
-	new MangroveSymbol('Int8', new SymbolType(SymbolTypes.type | SymbolTypes.integer | SymbolTypes.int8Bit)),
-	new MangroveSymbol('Int16', new SymbolType(SymbolTypes.type | SymbolTypes.integer | SymbolTypes.int16Bit)),
-	new MangroveSymbol('Int32', new SymbolType(SymbolTypes.type | SymbolTypes.integer | SymbolTypes.int32Bit)),
-	new MangroveSymbol('Int64', new SymbolType(SymbolTypes.type | SymbolTypes.integer | SymbolTypes.int64Bit)),
+	new MangroveSymbol('Bool', new SymbolType(SymbolTypes.type | SymbolTypes.bool)),
+	new MangroveSymbol('Int8', new SymbolType(SymbolTypes.type | SymbolTypes.signed | SymbolTypes.int8Bit)),
+	new MangroveSymbol('Int16', new SymbolType(SymbolTypes.type | SymbolTypes.signed | SymbolTypes.int16Bit)),
+	new MangroveSymbol('Int32', new SymbolType(SymbolTypes.type | SymbolTypes.signed | SymbolTypes.int32Bit)),
+	new MangroveSymbol('Int64', new SymbolType(SymbolTypes.type | SymbolTypes.signed | SymbolTypes.int64Bit)),
 	new MangroveSymbol('UInt8', new SymbolType(SymbolTypes.type | SymbolTypes.unsigned | SymbolTypes.int8Bit)),
 	new MangroveSymbol('UInt16', new SymbolType(SymbolTypes.type | SymbolTypes.unsigned | SymbolTypes.int16Bit)),
 	new MangroveSymbol('UInt32', new SymbolType(SymbolTypes.type | SymbolTypes.unsigned | SymbolTypes.int32Bit)),
