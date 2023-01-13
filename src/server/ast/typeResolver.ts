bc12d1eae89f160f647ef42eb0a80d27287fa629
dragonmux <git@dragonmux.network>
2023-01-13 21:54:47 +0000

    server/ast: Added handling of dotted identifiers to the type resolver

--- src/server/ast/typeResolver.ts
+++ src/server/ast/typeResolver.ts
@@ -13,7 +13,7 @@ export class TypeResolver
 	resolve(value: ASTNode): SymbolType | undefined
 	{
 		// Handle some of the simple cases first
-		if (value.type == ASTType.ident)
+		if (value.type == ASTType.ident || value.type == ASTType.dottedIdent)
 		{
 			const symbol = (value as ASTIdent).symbol
 			return symbol?.type
