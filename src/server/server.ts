d5598931dabf76e689f171a495ac0d81cf3ce2ca
Miika Alikirri <nykseli@protonmail.com>
2023-03-06 20:42:35 +0200

    server: Fixed server code based on new eslint rules

--- src/server/server.ts
+++ src/server/server.ts
@@ -23,13 +23,7 @@ connection.onInitialize((params: InitializeParams) =>
 	hasConfigurationCapability = !!(capabilities.workspace && capabilities.workspace.configuration)
 	hasWorkspaceFolderCapability = !!(capabilities.workspace && capabilities.workspace.workspaceFolders)
 
-	const result: InitializeResult =
-	{
-		capabilities:
-		{
-			completionProvider: {resolveProvider: true}
-		}
-	}
+	const result: InitializeResult = {capabilities: {completionProvider: {resolveProvider: true}}}
 	if (hasWorkspaceFolderCapability)
 		result.capabilities.workspace = {workspaceFolders: {supported: true}}
 	return result
